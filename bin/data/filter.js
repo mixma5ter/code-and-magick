775521ee89dad458a4c9e9c201889378938f5435
Funky-Juice <isa86box@gmail.com>
2016-11-07 10:41:46 +0300

    выполнил задание module7-task1

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
+  var newList = [];
+  var lastThreeDays = Date.now() - 1000 * 3600 * 24 * 3;
+  switch (filterID) {
+    case 'reviews-all':
+      newList = list;
+      break;
+    case 'reviews-recent':
+      newList = list.filter(function (listItem) {
+        return listItem.created <= lastThreeDays;
+      }).sort(function (a, b) {
+        return b.created - a.created;
+      });
+      break;
+    case 'reviews-good':
+      newList = list.filter(function(listItem) {
+        return listItem.rating >= 3;
+      }).sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+    case 'reviews-bad':
+      newList = list.filter(function(listItem) {
+        return listItem.rating < 3;
+      }).sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+      break;
+    case 'reviews-popular':
+      newList = list.slice().sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+      break;
+  }
+
+  return newList;
 };
