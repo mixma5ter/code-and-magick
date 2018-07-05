0f3a6f65c2260db018c7675368643a0e65ff0a4b
mixma5ter <deejay999@yandex.ru>
2018-07-05 13:18:39 +0300

    сделал первое задание

---
+++ src/js/check.js
@@ -0,0 +1,28 @@
+var getMessage = function (a, b) {
+  if (typeof a === 'boolean') {
+    if (a === true) {
+      return 'Я попал в ' + b;
+    }
+    if (a === false) {
+      return 'Я никуда не попал';
+    }
+  }
+  if (typeof a === 'number') {
+    return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  }
+  if (typeof a === 'object' && typeof b !== 'object') {
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps += a[i];
+    }
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
+  }
+  if (typeof a === 'object' && typeof b === 'object') {
+    var distancePath = 0;
+    for (var i = 0; i < a.length; i++) {
+      var productAB = a[i] * b[i];
+      distancePath += productAB;
+    }
+    return 'Я прошёл ' + distancePath + ' метров';
+  }
+};
