ac3d265dcfb102e9463f109c4b96612b6a3d482e
mixma5ter <deejay999@yandex.ru>
2018-07-11 16:27:06 +0300

    глобальные правки

--- src/js/form.js
+++ src/js/form.js
@@ -3,46 +3,27 @@
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
-  var reviesSubmitBtn = formContainer.querySelector('.review-submit');
-  var reviewName = document.getElementById('review-name');
-  var reviewText = document.getElementById('review-text');
   var reviewMark = formContainer.querySelectorAll('input[name=review-mark]');
   var reviewMarkValue;
   var reviewMarkIndex;
+  var reviewName = document.getElementById('review-name');
+  var reviewText = document.getElementById('review-text');
   var reviewFields = formContainer.querySelector('.review-fields');
   var reviewFieldsName = formContainer.querySelector('.review-fields-name');
   var reviewFieldsText = formContainer.querySelector('.review-fields-text');
-  var now = new Date();
-  var grace = new Date('1991-10-09');
-  var deleteCookie = ((now - grace) / (24 * 60 * 60 * 1000));
+  var reviesSubmitBtn = formContainer.querySelector('.review-submit');
 
-  var form = {
-    onClose: null,
-
-    /**
-     * @param {Function} cb
-     */
-    open: function(cb) {
-      formContainer.classList.remove('invisible');
-      reviewName.value = Cookies.get('review-name');
+  var checkValidityOpen = function() {
+    if (Cookies.get('review-mark-index')) {
       reviewMark[(Cookies.get('review-mark-index'))].checked = true;
-      cb();
-    },
+    }
 
-    close: function() {
-      formContainer.classList.add('invisible');
-
-      if (typeof this.onClose === 'function') {
-        this.onClose();
-      }
+    if (Cookies.get('review-name')) {
+      reviewName.value = Cookies.get('review-name');
     }
   };
 
-  reviesSubmitBtn.disabled = true;
-
-  formContainer.oninput = function(evt) {
-    evt.preventDefault();
-
+  var checkValidityInput = function() {
     for (var i = 0; i < reviewMark.length; i++) {
       if (reviewMark[i].checked) {
         reviewMarkValue = reviewMark[i].value;
@@ -77,10 +58,50 @@ window.form = (function() {
     }
   };
 
-  reviesSubmitBtn.onclick = function() {
+  var form = {
+    onClose: null,
+
+    /**
+     * @param {Function} cb
+     */
+    open: function(cb) {
+      formContainer.classList.remove('invisible');
+      checkValidityOpen();
+      checkValidityInput();
+      cb();
+    },
+
+    close: function() {
+      formContainer.classList.add('invisible');
+
+      if (typeof this.onClose === 'function') {
+        this.onClose();
+      }
+    }
+  };
+
+  var setCookie = function() {
+    var dateNow = new Date();
+    var birthDate = new Date('1906-12-09');
+    birthDate.setFullYear(dateNow.getFullYear());
+    if (birthDate >= dateNow) {
+      birthDate.setFullYear(dateNow.getFullYear() - 1);
+    }
+    var deleteCookie = ((dateNow - birthDate) / (24 * 60 * 60 * 1000));
     Cookies.set('review-mark', reviewMarkValue, {expires: deleteCookie});
     Cookies.set('review-mark-index', reviewMarkIndex, {expires: deleteCookie});
     Cookies.set('review-name', reviewName.value, {expires: deleteCookie});
+  };
+
+  formContainer.oninput = function(evt) {
+    evt.preventDefault();
+    checkValidityInput();
+  };
+
+  reviesSubmitBtn.onclick = function(evt) {
+    evt.preventDefault();
+    setCookie();
+    form.close();
   };
 
   formCloseButton.onclick = function(evt) {
