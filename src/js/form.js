aabc872f8071569bcdc037a398ec272263e79b72
mixma5ter <deejay999@yandex.ru>
2018-07-11 11:03:27 +0300

    создал функцию для записи cookie

--- src/js/form.js
+++ src/js/form.js
@@ -28,9 +28,13 @@ window.form = (function() {
   var reviewName = document.getElementById('review-name');
   var reviewText = document.getElementById('review-text');
   var reviewMark = formContainer.querySelectorAll('input[name=review-mark]');
+  var reviewMarkValue;
   var reviewFields = formContainer.querySelector('.review-fields');
   var reviewFieldsName = formContainer.querySelector('.review-fields-name');
   var reviewFieldsText = formContainer.querySelector('.review-fields-text');
+  var now = new Date();
+  var grace = new Date('1991-10-09');
+  var deleteCookie = ((now - grace) / (24 * 60 * 60 * 1000));
 
   reviesSubmitBtn.disabled = true;
 
@@ -39,7 +43,7 @@ window.form = (function() {
 
     for (var i = 0; i < reviewMark.length; i++) {
       if (reviewMark[i].checked) {
-        var reviewMarkValue = reviewMark[i].value;
+        reviewMarkValue = reviewMark[i].value;
       }
     }
 
@@ -68,6 +72,11 @@ window.form = (function() {
     } else {
       reviewFields.classList.remove('invisible');
     }
+  };
+
+  reviesSubmitBtn.onclick = function() {
+    Cookies.set('review-mark', reviewMarkValue, { expires: deleteCookie });
+    Cookies.set('review-name', reviewName.value, { expires: deleteCookie });
   };
 
   formCloseButton.onclick = function(evt) {
