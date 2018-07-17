185950afbb7620ac0fe2fb568a0aa9c91240f69e
mixma5ter <deejay999@yandex.ru>
2018-07-17 21:32:45 +0300

    работаю с отзывами

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,141 +1,166 @@
+/* eslint-disable no-shadow */
 'use strict';
 
 var reviews = [{
-  "author": {
-    "name": "Иванов Иван",
-    "picture": "img/user-1.jpg"
+  'author': {
+    'name': 'Иванов Иван',
+    'picture': 'img/user-1.jpg'
   },
-  "review_usefulness": 10,
-  "rating": 2,
-  "description": "Плохая игра: слишком сильно затягивает и невозможно оторваться. Я потерял работу, учебу, девушку и дар речи, но продолжаю играть. Это призыв о помощи: спасите."
+  'review_usefulness': 10,
+  'rating': 2,
+  'description': 'Плохая игра: слишком сильно затягивает и невозможно оторваться. Я потерял работу, учебу, девушку и дар речи, но продолжаю играть. Это призыв о помощи: спасите.'
 }, {
-  "author": {
-    "name": "Ксения Собчак",
-    "picture": "img/user-3.png"
+  'author': {
+    'name': 'Ксения Собчак',
+    'picture': 'img/user-3.png'
   },
-  "review_usefulness": 6,
-  "rating": 5,
-  "description": "Все хорошо, мне нравится."
+  'review_usefulness': 6,
+  'rating': 5,
+  'description': 'Все хорошо, мне нравится.'
 }, {
-  "author": {
-    "name": "Ксюша Бородина",
-    "picture": "img/user-2.png"
+  'author': {
+    'name': 'Ксюша Бородина',
+    'picture': 'img/user-2.png'
   },
-  "review_usefulness": 3,
-  "rating": 1,
-  "description": "Все плохо, мне не нравится"
+  'review_usefulness': 3,
+  'rating': 1,
+  'description': 'Все плохо, мне не нравится'
 }, {
-  "author": {
-    "name": "Мария Антуанетта",
-    "picture": "img/user-1.jpg"
+  'author': {
+    'name': 'Мария Антуанетта',
+    'picture': 'img/user-1.jpg'
   },
-  "review_usefulness": 4,
-  "rating": 3,
-  "description": "Невероятно чумовая игра. Пендальф-синий — мой герой)))) Он такой милашка. Благодаря ему я наконец нацчилась отвлекаться от работы и учёбы."
+  'review_usefulness': 4,
+  'rating': 3,
+  'description': 'Невероятно чумовая игра. Пендальф-синий — мой герой)))) Он такой милашка. Благодаря ему я наконец нацчилась отвлекаться от работы и учёбы.'
 }, {
-  "author": {
-    "name": "Дмитрий Карпов",
-    "picture": "img/user-3.png"
+  'author': {
+    'name': 'Дмитрий Карпов',
+    'picture': 'img/user-3.png'
   },
-  "review_usefulness": 20,
-  "rating": 4,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 20,
+  'rating': 4,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Максим Шаровары",
-    "picture": "img/user-1.jpg"
+  'author': {
+    'name': 'Максим Шаровары',
+    'picture': 'img/user-1.jpg'
   },
-  "review_usefulness": 115,
-  "rating": 2,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 115,
+  'rating': 2,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Зулейха Валиева",
-    "picture": "img/user-3.png"
+  'author': {
+    'name': 'Зулейха Валиева',
+    'picture': 'img/user-3.png'
   },
-  "review_usefulness": 10,
-  "rating": 4,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 10,
+  'rating': 4,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Федор Непомнящих",
-    "picture": "img/user-2.png"
+  'author': {
+    'name': 'Федор Непомнящих',
+    'picture': 'img/user-2.png'
   },
-  "review_usefulness": 10,
-  "rating": 3,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 10,
+  'rating': 3,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Макаронный Монстр",
-    "picture": "img/user-1.jpg"
+  'author': {
+    'name': 'Макаронный Монстр',
+    'picture': 'img/user-1.jpg'
   },
-  "review_usefulness": -3,
-  "rating": 5,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': -3,
+  'rating': 5,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Миклухо Маклай",
-    "picture": "img/user-3.png"
+  'author': {
+    'name': 'Миклухо Маклай',
+    'picture': 'img/user-3.png'
   },
-  "review_usefulness": 0,
-  "rating": 2,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 0,
+  'rating': 2,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Муравьев Апостол",
-    "picture": "img/user-2.png"
+  'author': {
+    'name': 'Муравьев Апостол',
+    'picture': 'img/user-2.png'
   },
-  "review_usefulness": 0,
-  "rating": 1,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 0,
+  'rating': 1,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Максим Горький",
-    "picture": "img/user-3.png"
+  'author': {
+    'name': 'Максим Горький',
+    'picture': 'img/user-3.png'
   },
-  "review_usefulness": 8,
-  "rating": 3,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 8,
+  'rating': 3,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Аноним",
-    "picture": "img/ijwdoq"
+  'author': {
+    'name': 'Аноним',
+    'picture': 'img/ijwdoq'
   },
-  "review_usefulness": 102,
-  "rating": 3,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 102,
+  'rating': 3,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Иван Иванов",
-    "picture": "img/user-1.jpg"
+  'author': {
+    'name': 'Иван Иванов',
+    'picture': 'img/user-1.jpg'
   },
-  "review_usefulness": 5,
-  "rating": 4,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 5,
+  'rating': 4,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Василиса Васильева",
-    "picture": "img/user-2.png"
+  'author': {
+    'name': 'Василиса Васильева',
+    'picture': 'img/user-2.png'
   },
-  "review_usefulness": 0,
-  "rating": 4,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 0,
+  'rating': 4,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Хороший Человек",
-    "picture": "img/user-2.png"
+  'author': {
+    'name': 'Хороший Человек',
+    'picture': 'img/user-2.png'
   },
-  "review_usefulness": 24,
-  "rating": 3,
-  "description": "Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди."
+  'review_usefulness': 24,
+  'rating': 3,
+  'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
 }, {
-  "author": {
-    "name": "Гейб Ньюэлл",
-    "picture": "img/dwjiqo9"
+  'author': {
+    'name': 'Гейб Ньюэлл',
+    'picture': 'img/dwjiqo9'
   },
-  "review_usefulness": 10,
-  "rating": 5,
-  "description": "Игра очень интересная. Нравится возможность выбирать между героями, а самое крутое, что есть альтернативные концовки в игре. Она точно стоит своих денег."
+  'review_usefulness': 10,
+  'rating': 5,
+  'description': 'Игра очень интересная. Нравится возможность выбирать между героями, а самое крутое, что есть альтернативные концовки в игре. Она точно стоит своих денег.'
 }];
 
-console.log(reviews);
+//var reviewsFilter = document.querySelector('.reviews-filter');
+var container = document.querySelector('.reviews-list');
+var template = document.getElementById('review-template');
+var templateContainer = 'content' in template ? template.content : template;
+
+reviews.forEach(function(reviews) {
+  var reviewsItem = templateContainer.querySelector('.review').cloneNode(true);
+  reviewsItem.querySelector('.review-author').textContent = reviews.author.name;
+  reviewsItem.querySelector('.review-rating').textContent = reviews.rating;
+  reviewsItem.querySelector('.review-text').textContent = reviews.description;
+
+  var authorImg = new Image(124, 124);
+  authorImg.onload = function() {
+    reviewsItem.querySelector('.review-author').src = reviews.author.picture;
+  };
+  authorImg.onerror = function() {
+    reviewsItem.classList.add('review-load-failure');
+  };
+  //authorImg.src = reviews.author.picture;
+  reviewsItem.querySelector('.review-author').src = reviews.author.picture;
+
+  container.appendChild(reviewsItem);
+});
+
+console.log(container);
